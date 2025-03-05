0f931f5cb1705815497e250e7c9dc718c608781c
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-05 23:57:56 +0900

    스프링 부트 프로젝트 초기화 (#3)
    * chore: 기본 제공된 gitignore -> gitignore.io 에서 만든 파일로 변경
    
    * build: application.yaml 파일 작성
    
    내용이 많지 않아 개발 환경과 테스트 환경 파일을 모아서 작성함. 나중에 파일이 커지거나 배포할 즈음에 분리할 예정.
    
    * chore: google-java-format 으로 코드 정렬
    
    * feat: 초기 개발 편의성을 위한 Security 설정
    
    기본 인증 상태를 모두 허용으로 바꿔서 개발 도중에 관찰을 발해하지 않도록 함. 이 설정은 그대로 두면 안 되고, 인증 구현을 할 때 적절히 다시 세팅되어야 함.
    
    * docs: 기본 리드미 작성

--- src/main/java/io/jeidiiy/sirenordersystem/SirenOrderSystemApplication.java
+++ src/main/java/io/jeidiiy/sirenordersystem/SirenOrderSystemApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SirenOrderSystemApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SirenOrderSystemApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SirenOrderSystemApplication.class, args);
+  }
 }
