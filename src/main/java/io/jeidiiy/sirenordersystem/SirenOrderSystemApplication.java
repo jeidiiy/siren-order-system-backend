931d3650dd8598eebe653bd98f17bf20b72aa015
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-05 22:08:06 +0900

    Inital Commit

---
+++ src/main/java/io/jeidiiy/sirenordersystem/SirenOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package io.jeidiiy.sirenordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirenOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SirenOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/jeidiiy/sirenordersystem/SirenOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jeidiiy.sirenordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirenOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
