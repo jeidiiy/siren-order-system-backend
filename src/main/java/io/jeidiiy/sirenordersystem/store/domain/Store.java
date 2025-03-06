7f63b28d8f1c6734934423a34eb8116b36200ab2
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-06 23:13:09 +0900

    #2 - 매장 엔티티 및 픽업 옵션 관련 엔티티 개발

---
+++ src/main/java/io/jeidiiy/sirenordersystem/store/domain/Store.java
@@ -0,0 +1,56 @@
+package io.jeidiiy.sirenordersystem.store.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(name = "stores")
+@Entity
+public class Store {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer storeId;
+
+  @Setter @Column private String name;
+
+  @Setter
+  @Column(length = 11)
+  private String contactNumber;
+
+  @Setter
+  @Column(length = 10)
+  private String openAt;
+
+  @Setter
+  @Column(length = 10)
+  private String closeAt;
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    Store store = (Store) o;
+
+    if (store.getStoreId() != null) {
+      return Objects.equals(getStoreId(), store.getStoreId());
+    }
+
+    return Objects.equals(getName(), store.getName())
+        && Objects.equals(getContactNumber(), store.getContactNumber())
+        && Objects.equals(getOpenAt(), store.getOpenAt())
+        && Objects.equals(getCloseAt(), store.getCloseAt());
+  }
+
+  @Override
+  public int hashCode() {
+    if (getStoreId() != null) {
+      return Objects.hash(getStoreId());
+    }
+
+    return Objects.hash(getName(), getContactNumber(), getOpenAt(), getCloseAt());
+  }
+}
