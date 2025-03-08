0cdda1594f740d1833394db3ae74f5f7001d92ac
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-09 00:17:17 +0900

    #15 - feat: Refresh Token 서비스 로직 및 관련 예외 클래스 개발

---
+++ src/main/java/io/jeidiiy/sirenordersystem/jwt/exception/ValidAccessTokenException.java
@@ -0,0 +1,7 @@
+package io.jeidiiy.sirenordersystem.jwt.exception;
+
+public class ValidAccessTokenException extends RuntimeException {
+    public ValidAccessTokenException(String message) {
+        super(message);
+    }
+}
