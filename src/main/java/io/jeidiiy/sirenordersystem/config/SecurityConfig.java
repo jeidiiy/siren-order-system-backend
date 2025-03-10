08b706bb5869e1d39a1a64ec753f3ff0dcc27322
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-10 17:13:22 +0900

    #12 - feat: 메소드 기반 인증 및 인가 기능 활성화

--- src/main/java/io/jeidiiy/sirenordersystem/config/SecurityConfig.java
+++ src/main/java/io/jeidiiy/sirenordersystem/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import io.jeidiiy.sirenordersystem.jwt.service.JwtLogoutSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
