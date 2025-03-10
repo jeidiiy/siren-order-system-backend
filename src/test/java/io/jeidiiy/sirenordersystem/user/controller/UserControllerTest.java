62427676da9a12e0de0fef041a3d172cbfad128a
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-10 21:49:54 +0900

    #12 - test: User CRUD 테스트 작성

--- src/test/java/io/jeidiiy/sirenordersystem/user/controller/UserControllerTest.java
+++ src/test/java/io/jeidiiy/sirenordersystem/user/controller/UserControllerTest.java
@@ -1,19 +1,20 @@
 package io.jeidiiy.sirenordersystem.user.controller;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jeidiiy.sirenordersystem.config.SecurityConfig;
 import io.jeidiiy.sirenordersystem.jwt.filter.JwtAuthenticationFilter;
+import io.jeidiiy.sirenordersystem.jwt.model.JwtToken;
 import io.jeidiiy.sirenordersystem.jwt.service.JwtAuthenticationEntryPoint;
 import io.jeidiiy.sirenordersystem.jwt.service.JwtLogoutSuccessHandler;
 import io.jeidiiy.sirenordersystem.jwt.service.JwtService;
+import io.jeidiiy.sirenordersystem.user.domain.dto.UserPatchRequestBody;
 import io.jeidiiy.sirenordersystem.user.domain.dto.UserPostRequestBody;
 import io.jeidiiy.sirenordersystem.user.exception.UserAlreadyExistsException;
 import io.jeidiiy.sirenordersystem.user.service.UserService;
@@ -23,23 +24,60 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @DisplayName("[Controller] 사용자 컨트롤러 테스트")
-@Import({SecurityConfig.class, JwtAuthenticationFilter.class})
+@Import({SecurityConfig.class, JwtAuthenticationFilter.class, JwtService.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @WebMvcTest(UserController.class)
 class UserControllerTest {
   @Autowired MockMvc mvc;
   @Autowired ObjectMapper mapper;
+  @Autowired JwtService jwtService;
 
   @MockitoBean UserService userService;
-  @MockitoBean JwtService jwtService;
   @MockitoBean JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
   @MockitoBean JwtLogoutSuccessHandler jwtLogoutSuccessHandler;
+
+  @DisplayName("[GET] 로그인한 유저가 자기 정보 조회 -> 200 OK (정상)")
+  @WithMockUser("loginUser")
+  @Test
+  void givenAuthenticationUserAndSameUsername_whenRequesting_thenRespondsUser() throws Exception {
+    // given
+    String username = "loginUser";
+    JwtToken jwtToken = jwtService.generateJwtToken(username);
+
+    // when & then
+    mvc.perform(
+            get("/api/v1/users/{username}", username)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken.accessToken()))
+        .andExpect(status().isOk());
+    then(userService).should().getUserByUsername(username);
+  }
+
+  @DisplayName("[GET] 로그인한 유저가 다른 사람 정보 조회 -> 403 Forbidden (실패)")
+  @WithMockUser("testUser")
+  @Test
+  void givenAuthenticationUserAndDifferentUsername_whenRequesting_thenResponds403()
+      throws Exception {
+    // given
+    String anotherUsername = "anotherUser";
+    JwtToken jwtToken = jwtService.generateJwtToken(anotherUsername);
+
+    // when & then
+    mvc.perform(
+            get("/api/v1/users/{username}", anotherUsername)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken.accessToken()))
+        .andExpect(status().isForbidden());
+    then(userService).shouldHaveNoInteractions();
+  }
 
   @DisplayName("[POST] 존재하지 않는 사용자 이름으로 회원가입 시도 -> 201 Created (정상)")
   @Test
@@ -189,6 +227,89 @@ class UserControllerTest {
                 .content(mapper.writeValueAsString(userPostRequestBody)))
         .andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.message").value(Matchers.containsString("닉네임은 1~6자의 한글만 가능합니다.")));
+    then(userService).shouldHaveNoInteractions();
+  }
+
+  @DisplayName("[PATCH] 로그인한 유저가 자기 정보 수정 -> 200 OK (정상)")
+  @WithMockUser("loginUser")
+  @Test
+  void givenAuthenticationUserAndSameUsername_whenUpdating_thenResponds200() throws Exception {
+    // given
+    String username = "loginUser";
+    JwtToken jwtToken = jwtService.generateJwtToken(username);
+    UserPatchRequestBody userPatchRequestBody =
+        UserPatchRequestBody.builder()
+            .nickname("테스트닉네임")
+            .password("password11!!")
+            .realname("테스트찐이름")
+            .username(username)
+            .build();
+
+    // when & then
+    mvc.perform(
+            patch("/api/v1/users/{username}", username)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken.accessToken())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(userPatchRequestBody)))
+        .andExpect(status().isOk());
+    then(userService).should().updateUserByUsername(username, userPatchRequestBody);
+  }
+
+  @DisplayName("[PATCH] 로그인한 유저가 다른 사람 정보 수정 -> 403 Forbidden (실패)")
+  @WithMockUser("loginUser")
+  @Test
+  void givenAuthenticationUserAndAnotherUsername_whenUpdating_thenResponds403() throws Exception {
+    // given
+    String anotherUsername = "anotherUser";
+    JwtToken jwtToken = jwtService.generateJwtToken(anotherUsername);
+    UserPatchRequestBody userPatchRequestBody =
+        UserPatchRequestBody.builder()
+            .nickname("테스트닉네임")
+            .password("password11!!")
+            .realname("테스트찐이름")
+            .username(anotherUsername)
+            .build();
+
+    // when & then
+    mvc.perform(
+            delete("/api/v1/users/{username}", anotherUsername)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken.accessToken())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(userPatchRequestBody)))
+        .andExpect(status().isForbidden());
+    then(userService).shouldHaveNoInteractions();
+  }
+
+  @DisplayName("[DELETE] 로그인한 유저가 회원 탈퇴 -> 200 OK (정상)")
+  @WithMockUser("loginUser")
+  @Test
+  void givenAuthenticationUserAndSameUsername_whenDeleting_thenResponds200() throws Exception {
+    // given
+    String username = "loginUser";
+    JwtToken jwtToken = jwtService.generateJwtToken(username);
+
+    // when & then
+    mvc.perform(
+            delete("/api/v1/users/{username}", username)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken.accessToken()))
+        .andExpect(status().isOk());
+    then(userService).should().deleteUserByUsername(username);
+  }
+
+  @DisplayName("[DELETE] 로그인한 유저가 다른 사람 정보 삭제 -> 403 Forbidden (실패)")
+  @WithMockUser("loginUser")
+  @Test
+  void givenAuthenticationUserAndAnotherUsername_whenDeleting_thenResponds403() throws Exception {
+    // given
+    String anotherUsername = "anotherUser";
+    JwtToken jwtToken = jwtService.generateJwtToken(anotherUsername);
+
+    // when & then
+    mvc.perform(
+            delete("/api/v1/users/{username}", anotherUsername)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken.accessToken())
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isForbidden());
     then(userService).shouldHaveNoInteractions();
   }
 }
