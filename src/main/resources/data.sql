dc310b49cbd5059636464849fe9742de70af89f9
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-10 18:00:12 +0900

    #12 - chore: 로그인 및 사용자 CRUD API 테스트용 파일 작성, 데이터 추가
    Signup.http 내용을 User.http 에 포함시킴

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO users (user_id, nickname, real_name, password, username, role)
-VALUES (1, '테스트닉네임', '테스트찐이름', '$2a$10$RB7reqJVJ85bm1Gtioang.AtegelGbJD/8eO5RufXq3KT/gbNxEWq', 'jeidiiy', 'CUSTOMER');
+INSERT INTO users (user_id, nickname, realname, password, username, role)
+VALUES (1, '진격의거인', '에렌', '$2a$10$lb5Qtp1X7NA8azxkigB0E.AvNoL1sN0QgxSaJlTzrldCq5duv03Ay', 'eren', 'CUSTOMER'),
+       (2, '초대형거인', '아르민', '$2a$10$lb5Qtp1X7NA8azxkigB0E.AvNoL1sN0QgxSaJlTzrldCq5duv03Ay', 'armin', 'CUSTOMER');
