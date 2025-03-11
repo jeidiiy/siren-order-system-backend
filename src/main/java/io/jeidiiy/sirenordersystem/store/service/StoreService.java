4ab020301187fc16c7ab5b45cc3e396fdc0c8245
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-11 23:39:49 +0900

    #21 - feat: 매장 서비스 로직 개발

---
+++ src/main/java/io/jeidiiy/sirenordersystem/store/service/StoreService.java
@@ -0,0 +1,56 @@
+package io.jeidiiy.sirenordersystem.store.service;
+
+import io.jeidiiy.sirenordersystem.store.domain.Store;
+import io.jeidiiy.sirenordersystem.store.domain.dto.StorePutRequestBody;
+import io.jeidiiy.sirenordersystem.store.domain.dto.StoreResponseDto;
+import io.jeidiiy.sirenordersystem.store.exception.StoreNotFoundException;
+import io.jeidiiy.sirenordersystem.store.repository.StoreJpaRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class StoreService {
+
+  private final StoreJpaRepository storeJpaRepository;
+
+  @Transactional(readOnly = true)
+  public List<StoreResponseDto> findStores() {
+    return storeJpaRepository.findAll().stream()
+        .map(StoreResponseDto::from)
+        .collect(Collectors.toList());
+  }
+
+  @Transactional(readOnly = true)
+  public StoreResponseDto findStoreById(Integer storeId) {
+    return StoreResponseDto.from(
+        storeJpaRepository
+            .findById(storeId)
+            .orElseThrow(() -> new StoreNotFoundException(storeId)));
+  }
+
+  public void toggleStoreIsOpenByStoreIdAndLoginUserUsername(
+      Integer storeId, String currentUserUsername) {
+    Store store = findStoreByStoreIdAndUserUsername(storeId, currentUserUsername);
+    store.setIsOpen(!store.getIsOpen());
+    storeJpaRepository.save(store);
+  }
+
+  @Transactional(readOnly = true)
+  public Store findStoreByStoreIdAndUserUsername(Integer storeId, String currentUserUsername) {
+    return storeJpaRepository
+        .findByStoreIdAndUserUsername(storeId, currentUserUsername)
+        .orElseThrow(() -> new StoreNotFoundException(storeId, currentUserUsername));
+  }
+
+  public void updateStoreByStoreIdAndLoginUserUsername(
+      StorePutRequestBody requestBody, Integer storeId, String currentUsername) {
+    Store store = findStoreByStoreIdAndUserUsername(storeId, currentUsername);
+    store.update(requestBody);
+    storeJpaRepository.save(store);
+  }
+}
