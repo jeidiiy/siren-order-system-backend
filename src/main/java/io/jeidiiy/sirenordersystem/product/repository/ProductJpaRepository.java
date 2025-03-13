fb09cff34629dc4a7c3799afeb19c41f41e99d83
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-13 21:01:27 +0900

    #20 - feat: Product 레포지토리 구현 및 커스텀 쿼리 작성

---
+++ src/main/java/io/jeidiiy/sirenordersystem/product/repository/ProductJpaRepository.java
@@ -0,0 +1,13 @@
+package io.jeidiiy.sirenordersystem.product.repository;
+
+import io.jeidiiy.sirenordersystem.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductJpaRepository extends JpaRepository<Product, Integer> {
+  @Query("SELECT p FROM Product p JOIN TypeProduct tp ON tp.product.productId = p.productId WHERE tp.type.typeId = :typeId")
+  List<Product> findAllByTypeProductsTypeId(@Param("typeId") Integer typeId);
+}
