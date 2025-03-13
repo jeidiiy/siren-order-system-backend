e168ee42460735c6d68862d2b6a7748a7dfb7ae2
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-13 21:01:41 +0900

    #20 - feat: Product 서비스 코드 개발

---
+++ src/main/java/io/jeidiiy/sirenordersystem/product/service/ProductService.java
@@ -0,0 +1,25 @@
+package io.jeidiiy.sirenordersystem.product.service;
+
+import io.jeidiiy.sirenordersystem.product.domain.Product;
+import io.jeidiiy.sirenordersystem.product.domain.dto.ProductResponseDto;
+import io.jeidiiy.sirenordersystem.product.repository.ProductJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ProductService {
+  private final ProductJpaRepository productJpaRepository;
+
+  public List<ProductResponseDto> findAllByTypeId(Integer typeId) {
+    List<Product> products = productJpaRepository.findAllByTypeProductsTypeId(typeId);
+    if (products.isEmpty()) {
+      throw new IllegalArgumentException("해당 종류에 해당하는 상품은 없습니다. Type Id : " + typeId);
+    }
+    return products.stream().map(ProductResponseDto::from).toList();
+  }
+}
