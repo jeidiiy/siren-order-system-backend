f677699fa7305bbd474adde75dadbcfcb1c8034d
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-17 23:36:34 +0900

    #19 - feat: 주문 요청을 위한 DTO 개발

---
+++ src/main/java/io/jeidiiy/sirenordersystem/order/domain/dto/BeverageOptionDto.java
@@ -0,0 +1,20 @@
+package io.jeidiiy.sirenordersystem.order.domain.dto;
+
+import io.jeidiiy.sirenordersystem.product.domain.beverage.decorator.bean.BeanType;
+import io.jeidiiy.sirenordersystem.product.domain.beverage.decorator.cup.CupSize;
+import io.jeidiiy.sirenordersystem.product.domain.beverage.decorator.temperature.Level;
+import io.jeidiiy.sirenordersystem.product.domain.beverage.decorator.water.Amount;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Map;
+
+public record BeverageOptionDto(
+    @NotNull(message = "원두 종류는 필수값입니다") BeanType beanType,
+    @NotNull(message = "컵 크기는 필수값입니다") CupSize cupSize,
+    @NotNull(message = "샷 횟수는 필수값입니다") Integer shots,
+    Map<String, Integer> syrupOptions,
+    Amount water,
+    io.jeidiiy.sirenordersystem.product.domain.beverage.decorator.ice.Amount ice,
+    io.jeidiiy.sirenordersystem.product.domain.beverage.decorator.whippedcream.Amount whippedCream,
+    String etc,
+    Level temperature) {}
---
+++ src/main/java/io/jeidiiy/sirenordersystem/order/domain/dto/MerchandiseOptionDto.java
@@ -0,0 +1,5 @@
+package io.jeidiiy.sirenordersystem.order.domain.dto;
+
+import io.jeidiiy.sirenordersystem.product.domain.merchandise.decorator.packaging.PackagingOption;
+
+public record MerchandiseOptionDto(PackagingOption option) {}
