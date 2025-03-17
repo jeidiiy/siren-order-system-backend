668923b1e71596a9b4a6e456b23a3591ec5cab0c
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-17 23:42:22 +0900

    #19 - feat: Enum 옵션 값들에 대한 Jackson 매핑 처리

--- src/main/java/io/jeidiiy/sirenordersystem/product/domain/beverage/decorator/temperature/Level.java
+++ src/main/java/io/jeidiiy/sirenordersystem/product/domain/beverage/decorator/temperature/Level.java
@@ -1,6 +1,19 @@
 package io.jeidiiy.sirenordersystem.product.domain.beverage.decorator.temperature;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Level {
   WARM,
-  HOT
+  HOT;
+
+  @JsonCreator
+  public static Level forValue(String value) {
+    return Level.valueOf(value.toUpperCase());
+  }
+
+  @JsonValue
+  public String toJson() {
+    return name().toLowerCase();
+  }
 }
--- src/main/java/io/jeidiiy/sirenordersystem/product/domain/beverage/decorator/water/Amount.java
+++ src/main/java/io/jeidiiy/sirenordersystem/product/domain/beverage/decorator/water/Amount.java
@@ -1,6 +1,19 @@
 package io.jeidiiy.sirenordersystem.product.domain.beverage.decorator.water;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Amount {
   SOME,
-  ENOUGH
+  ENOUGH;
+
+  @JsonCreator
+  public static Amount forValue(String value) {
+    return Amount.valueOf(value.toUpperCase());
+  }
+
+  @JsonValue
+  public String toJson() {
+    return name().toLowerCase();
+  }
 }
