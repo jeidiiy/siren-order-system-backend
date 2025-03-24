1c35becce82516e1ab91a8bb9a3ab00b80cffa6a
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-24 23:02:50 +0900

    feat: TypeResponseDto에 id 필드 추가 (#45)

--- src/main/java/io/jeidiiy/sirenordersystem/product/domain/dto/TypeResponseDto.java
+++ src/main/java/io/jeidiiy/sirenordersystem/product/domain/dto/TypeResponseDto.java
@@ -2,8 +2,8 @@ package io.jeidiiy.sirenordersystem.product.domain.dto;
 
 import io.jeidiiy.sirenordersystem.product.domain.Type;
 
-public record TypeResponseDto(String title, String description) {
+public record TypeResponseDto(Integer id, String title, String description) {
   public static TypeResponseDto from(Type type) {
-    return new TypeResponseDto(type.getTitle(), type.getDescription());
+    return new TypeResponseDto(type.getId(), type.getTitle(), type.getDescription());
   }
 }
