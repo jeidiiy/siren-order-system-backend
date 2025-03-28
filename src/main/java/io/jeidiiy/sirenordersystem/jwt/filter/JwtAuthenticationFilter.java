6de81635a63647d7764823b76880ad01de710c3c
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-28 18:02:47 +0900

    #58 - feat: JWT 검증 및 DB에서 조회 중 예외 발생 시 req 에 저장

--- src/main/java/io/jeidiiy/sirenordersystem/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/io/jeidiiy/sirenordersystem/jwt/filter/JwtAuthenticationFilter.java
@@ -1,7 +1,13 @@
 package io.jeidiiy.sirenordersystem.jwt.filter;
 
+import io.jeidiiy.sirenordersystem.jwt.exception.IlligalSignatureException;
 import io.jeidiiy.sirenordersystem.jwt.service.JwtService;
 import io.jeidiiy.sirenordersystem.user.service.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,8 +15,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
@@ -36,14 +43,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         && securityContext.getAuthentication() == null
         && !request.getRequestURI().equals("/api/v1/refresh-token")) {
       var accessToken = authorization.substring(BEARER_PREFIX.length()).trim();
-      var username = jwtService.getUsername(accessToken);
-      var userDetails = userService.loadUserByUsername(username);
-      var authenticationToken =
-          new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-      authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+      String username;
+      try {
+        username = jwtService.getUsername(accessToken);
+        var userDetails = userService.loadUserByUsername(username);
+        var authenticationToken =
+            new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-      securityContext.setAuthentication(authenticationToken);
-      SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        securityContext.setAuthentication(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      } catch (JwtException | UsernameNotFoundException ex) {
+        // 예외를 request 에 저장 후 나중에 JwtAuthExceptionFilter 에서 처리
+        request.setAttribute("authException", ex);
+      }
     }
     filterChain.doFilter(request, response);
   }
