d5d4796dd94587da9bbfa8bca07758cf86d97eb5
JIYONG JUNG <jeidiiy98@gmail.com>
2025-04-08 13:26:38 +0900

    #66 - feat: 주문 내역 조회 API 개발

--- src/main/java/io/jeidiiy/sirenordersystem/order/controller/OrderController.java
+++ src/main/java/io/jeidiiy/sirenordersystem/order/controller/OrderController.java
@@ -1,19 +1,18 @@
 package io.jeidiiy.sirenordersystem.order.controller;
 
 import io.jeidiiy.sirenordersystem.order.domain.dto.OrderPostRequestBody;
+import io.jeidiiy.sirenordersystem.order.domain.dto.OrderResponseDto;
 import io.jeidiiy.sirenordersystem.order.service.OrderService;
 import io.jeidiiy.sirenordersystem.user.domain.dto.AuthenticationUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "주문 API")
 @RequiredArgsConstructor
@@ -31,5 +30,12 @@ public class OrderController {
       @Valid @RequestBody OrderPostRequestBody requestBody) {
     orderService.createOrder(requestBody, currentUser.getUsername());
     return ResponseEntity.ok().build();
+  }
+
+  @Operation(summary = "사용자 본인이 주문한 내역 조회하기")
+  @PreAuthorize("#username == authentication.name")
+  @GetMapping("/{username}")
+  public ResponseEntity<List<OrderResponseDto>> getOrders(@PathVariable String username) {
+    return ResponseEntity.ok(orderService.getOrderResponseDtoByCurrentUser(username));
   }
 }
