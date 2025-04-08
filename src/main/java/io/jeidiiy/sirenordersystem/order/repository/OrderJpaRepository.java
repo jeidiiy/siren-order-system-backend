2e5d7dd79b05c3bd8529f209cf3f302c80cf7e7d
JIYONG JUNG <jeidiiy98@gmail.com>
2025-04-08 14:27:13 +0900

    #66 - feat: 특정 주문 조회 레포지토리 코드 개발

--- src/main/java/io/jeidiiy/sirenordersystem/order/repository/OrderJpaRepository.java
+++ src/main/java/io/jeidiiy/sirenordersystem/order/repository/OrderJpaRepository.java
@@ -1,10 +1,12 @@
 package io.jeidiiy.sirenordersystem.order.repository;
 
 import io.jeidiiy.sirenordersystem.order.domain.Order;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface OrderJpaRepository extends JpaRepository<Order, Integer> {
-    List<Order> findAllByUserId(Integer userId);
+  List<Order> findAllByUserId(Integer userId);
+
+  Optional<Order> findByIdAndUserId(Integer orderId, Integer userId);
 }
