cfbf70921a47a8068ae6ace89d8c4986457ab669
JIYONG JUNG <jeidiiy98@gmail.com>
2025-04-14 18:36:30 +0900

    #72 - 장바구니 서비스 및 예외 개발

---
+++ src/main/java/io/jeidiiy/sirenordersystem/cart/service/CartService.java
@@ -0,0 +1,66 @@
+package io.jeidiiy.sirenordersystem.cart.service;
+
+import io.jeidiiy.sirenordersystem.cart.domain.Cart;
+import io.jeidiiy.sirenordersystem.cart.domain.dto.CartPostRequestDto;
+import io.jeidiiy.sirenordersystem.cart.domain.dto.CartResponseDto;
+import io.jeidiiy.sirenordersystem.cart.exception.NonExistCartException;
+import io.jeidiiy.sirenordersystem.cart.repository.CartJpaRepository;
+import io.jeidiiy.sirenordersystem.product.domain.Product;
+import io.jeidiiy.sirenordersystem.product.service.ProductService;
+import io.jeidiiy.sirenordersystem.user.domain.User;
+import io.jeidiiy.sirenordersystem.user.service.UserService;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class CartService {
+
+  private final CartJpaRepository cartJpaRepository;
+  private final UserService userService;
+  private final ProductService productService;
+
+  @Transactional(readOnly = true)
+  public List<CartResponseDto> findByUsername(String username) {
+    User user = userService.getUserByUsername(username);
+    List<Cart> carts = cartJpaRepository.findAllByUserId(user.getId());
+    return carts.stream().map(CartResponseDto::fromEntity).toList();
+  }
+
+  public List<CartResponseDto> upsert(String username, CartPostRequestDto cartPostRequestDto) {
+    if (cartPostRequestDto.cartId() == null) {
+      User user = userService.getUserByUsername(username);
+      Product product = productService.findById(cartPostRequestDto.productId());
+      cartJpaRepository.save(Cart.of(user, product));
+    } else {
+      Optional<Cart> optionalCart = cartJpaRepository.findById(cartPostRequestDto.cartId());
+
+      if (optionalCart.isEmpty()) {
+        throw new NonExistCartException("ID에 해당하는 장바구니가 없습니다. ID: " + cartPostRequestDto.cartId());
+      }
+
+      Cart cart = optionalCart.get();
+      Product product = productService.findById(cartPostRequestDto.productId());
+      cart.setQuantity(cartPostRequestDto.quantity());
+      cart.setPrice(product.getBasePrice() * cart.getQuantity());
+      cartJpaRepository.save(cart);
+    }
+
+    return findByUsername(username);
+  }
+
+  public List<CartResponseDto> remove(String username, Integer cartId) {
+    cartJpaRepository.deleteById(cartId);
+    return findByUsername(username);
+  }
+
+  public List<CartResponseDto> removeAll(String username) {
+    User user = userService.getUserByUsername(username);
+    cartJpaRepository.deleteAllByUserId(user.getId());
+    return findByUsername(username);
+  }
+}
