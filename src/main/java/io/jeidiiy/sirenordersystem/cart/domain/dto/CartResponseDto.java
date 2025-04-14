1519fd53dfdd6941121569100d14aa304a50d617
JIYONG JUNG <jeidiiy98@gmail.com>
2025-04-14 21:21:46 +0900

    feat: 장바구니 조회 API 응답에 상품 ID 추가 (#78)
    * #77 - feat: 장바구니 조회 API 응답에 상품 ID 추가
    
    * #77 - fix: 잘못된 코드 삭제
    
    상품 가격은 그대로이고 프론트단에서 계산하는 것이다.
    그런데 수량에 따라 가격도 변하게 만드는 코드가 있어 이를 삭제한다.

--- src/main/java/io/jeidiiy/sirenordersystem/cart/domain/dto/CartResponseDto.java
+++ src/main/java/io/jeidiiy/sirenordersystem/cart/domain/dto/CartResponseDto.java
@@ -4,10 +4,10 @@ import io.jeidiiy.sirenordersystem.cart.domain.Cart;
 import io.jeidiiy.sirenordersystem.product.domain.Product;
 
 public record CartResponseDto(
-    Integer cartId, Integer quantity, Integer price, String productKrName) {
+    Integer cartId, Integer productId, Integer quantity, Integer price, String productKrName) {
   public static CartResponseDto fromEntity(Cart cart) {
     Product product = cart.getProduct();
     return new CartResponseDto(
-        cart.getId(), cart.getQuantity(), cart.getPrice(), product.getKrName());
+        cart.getId(), product.getId(), cart.getQuantity(), cart.getPrice(), product.getKrName());
   }
 }
