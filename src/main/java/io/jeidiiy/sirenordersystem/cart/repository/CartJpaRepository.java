a412ea9705c173f39627dca061b134b380e6678c
JIYONG JUNG <jeidiiy98@gmail.com>
2025-04-15 18:26:59 +0900

    #79 - feat: 장바구니 삭제 시 N+1 문제 해결

--- src/main/java/io/jeidiiy/sirenordersystem/cart/repository/CartJpaRepository.java
+++ src/main/java/io/jeidiiy/sirenordersystem/cart/repository/CartJpaRepository.java
@@ -3,9 +3,13 @@ package io.jeidiiy.sirenordersystem.cart.repository;
 import io.jeidiiy.sirenordersystem.cart.domain.Cart;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartJpaRepository extends JpaRepository<Cart, Integer> {
   List<Cart> findAllByUserId(Integer userId);
 
+  @Modifying
+  @Query("DELETE FROM Cart c WHERE c.user.id = :userId")
   void deleteAllByUserId(Integer userId);
 }
