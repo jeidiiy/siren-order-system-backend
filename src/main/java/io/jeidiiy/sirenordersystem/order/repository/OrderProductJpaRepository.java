1aadd9dc227d7f36d6f4d8d472d6d96e9b350f9b
JIYONG JUNG <jeidiiy98@gmail.com>
2025-04-15 20:59:11 +0900

    #79 - feat: 주문 조회 N+1 문제 해결

--- src/main/java/io/jeidiiy/sirenordersystem/order/repository/OrderProductJpaRepository.java
+++ src/main/java/io/jeidiiy/sirenordersystem/order/repository/OrderProductJpaRepository.java
@@ -3,7 +3,9 @@ package io.jeidiiy.sirenordersystem.order.repository;
 import io.jeidiiy.sirenordersystem.order.domain.OrderProduct;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderProductJpaRepository extends JpaRepository<OrderProduct, Integer> {
+  @Query("SELECT DISTINCT op FROM OrderProduct op JOIN FETCH op.product WHERE op.order.id = :orderId")
   List<OrderProduct> findAllByOrderId(Integer orderId);
 }
